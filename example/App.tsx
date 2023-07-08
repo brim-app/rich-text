7087e5ad18ae18a947b4e0595a363cdc3c24b910
Muhammed Saeed <stringsaeed@gmail.com>
2023-07-09 02:39:13 +0300

    chore(example): reformat typescript files

--- example/App.tsx
+++ example/App.tsx
@@ -1,61 +1,57 @@
-import {SafeAreaView, StyleSheet} from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import {
-    BrimAppRichTextView,
-    BrimAppRichTextViewRef,
+  BrimAppRichTextView,
+  BrimAppRichTextViewRef,
 } from "@brim-app/rich-text";
-import {useCallback, useRef, useState} from "react";
+import { useCallback, useRef, useState } from "react";
 import Toolbar from "./Toolbar";
 
 export default function App() {
-    const ref = useRef<BrimAppRichTextViewRef>(null);
+  const ref = useRef<BrimAppRichTextViewRef>(null);
 
-    const [editorState, setEditorState] = useState<{
-        bold: boolean;
-        italic: boolean;
-        underline: boolean;
-        strikethrough: boolean;
-    }>({
-        bold: false,
-        italic: false,
-        underline: false,
-        strikethrough: false,
-    });
+  const [editorState, setEditorState] = useState<{
+    bold: boolean;
+    italic: boolean;
+    underline: boolean;
+    strikethrough: boolean;
+  }>({
+    bold: false,
+    italic: false,
+    underline: false,
+    strikethrough: false,
+  });
 
+  const handleState = useCallback(
+    (key: keyof typeof editorState) => {
+      return () => {
+        const currentValue = editorState[key];
+        setEditorState((state) => ({
+          ...state,
+          [key]: !currentValue,
+        }));
+        ref.current?.setStyle(key, !currentValue);
+      };
+    },
+    [editorState]
+  );
 
-    const handleState = useCallback(
-        (key: keyof typeof editorState) => {
-            return () => {
-                const currentValue = editorState[key];
-                setEditorState((state) => ({
-                    ...state,
-                    [key]: !currentValue,
-                }));
-                ref.current?.setStyle(key, !currentValue);
-            };
-        },
-        [editorState]
-    );
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <Toolbar editorState={editorState} handleState={handleState}/>
-            <BrimAppRichTextView
-                ref={ref}
-                style={styles.rtView}
-            />
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <Toolbar editorState={editorState} handleState={handleState} />
+      <BrimAppRichTextView ref={ref} style={styles.rtView} />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        paddingHorizontal: 16,
-    },
-    rtView: {
-        flex: 1,
-        width: "100%",
-        height: "100%",
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
+  },
+  rtView: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
 });
--- example/Toolbar.tsx
+++ example/Toolbar.tsx
@@ -1,70 +1,69 @@
 import React from "react";
-import {StyleSheet, View} from "react-native";
+import { StyleSheet, View } from "react-native";
 import Button from "./Button";
 import icons from "./icons";
 
 interface ToolbarProps {
-    editorState: {
-        bold: boolean;
-        italic: boolean;
-        underline: boolean;
-        strikethrough: boolean;
-    };
-    handleState: (key: keyof ToolbarProps['editorState']) => () => void;
+  editorState: {
+    bold: boolean;
+    italic: boolean;
+    underline: boolean;
+    strikethrough: boolean;
+  };
+  handleState: (key: keyof ToolbarProps["editorState"]) => () => void;
 }
 
-export default function Toolbar({handleState, editorState}: ToolbarProps) {
-
-    return (
-        <View style={styles.toolbar}>
-            <Button
-                source={icons['bold-regular']}
-                onPress={handleState('bold')}
-                selected={editorState.bold}
-            />
-            <Button
-                source={icons['italic-regular']}
-                onPress={handleState('italic')}
-                selected={editorState.italic}
-            />
-            <Button
-                source={icons['strikethrough-regular']}
-                onPress={handleState('strikethrough')}
-                selected={editorState.strikethrough}
-            />
-            <Button
-                source={icons['underline-regular']}
-                onPress={handleState('underline')}
-                selected={editorState.underline}
-            />
-        </View>
-    )
+export default function Toolbar({ handleState, editorState }: ToolbarProps) {
+  return (
+    <View style={styles.toolbar}>
+      <Button
+        source={icons["bold-regular"]}
+        onPress={handleState("bold")}
+        selected={editorState.bold}
+      />
+      <Button
+        source={icons["italic-regular"]}
+        onPress={handleState("italic")}
+        selected={editorState.italic}
+      />
+      <Button
+        source={icons["strikethrough-regular"]}
+        onPress={handleState("strikethrough")}
+        selected={editorState.strikethrough}
+      />
+      <Button
+        source={icons["underline-regular"]}
+        onPress={handleState("underline")}
+        selected={editorState.underline}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        paddingTop: 40,
-        paddingHorizontal: 20,
-    },
-    toolbar: {
-        flexDirection: 'row',
-        width: '100%',
-        flexWrap: 'wrap',
-        gap: 16,
-        alignItems: 'center',
-        justifyContent: 'center',
-        // light gray bg
-        backgroundColor: '#f5f5f5',
-    },
-    box: {
-        width: '100%',
-        minHeight: 60,
-        flexGrow: 1,
-        marginVertical: 20,
-        backgroundColor: 'white',
-        fontSize: 16,
-        fontFamily: 'monospace',
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  toolbar: {
+    flexDirection: "row",
+    width: "100%",
+    flexWrap: "wrap",
+    gap: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    // light gray bg
+    backgroundColor: "#f5f5f5",
+  },
+  box: {
+    width: "100%",
+    minHeight: 60,
+    flexGrow: 1,
+    marginVertical: 20,
+    backgroundColor: "white",
+    fontSize: 16,
+    fontFamily: "monospace",
+  },
 });
